03de212e9aae1693d7c978bd57dedb73effb859c
James Watterson <jameslwatterson@gmail.com>
2012-08-09 00:11:49 -0400

    MADE PURTIER BUTTONS ETC EDIT AND DELETE TODO:FIX STORAGE

--- VoiceJournal/src/com/watterso/noter/DatabaseHandler.java
+++ VoiceJournal/src/com/watterso/noter/DatabaseHandler.java
@@ -12,7 +12,7 @@ import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
 
 public class DatabaseHandler extends SQLiteOpenHelper {
-	private static final int DATABASE_VERSION = 5;
+	private static final int DATABASE_VERSION = 6;
  
     // Database Name
     private static final String DATABASE_NAME = "entryManager";
